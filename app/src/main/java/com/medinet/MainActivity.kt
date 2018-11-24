d9faee9f3ab852722f1e936e72c6d4c9f3858a71
Aleksandr Gornostal <al.gornostal@gmail.com>
2018-11-24 23:47:48 +0200

    now it scans QR codes

--- app/src/main/java/com/medinet/MainActivity.kt
+++ app/src/main/java/com/medinet/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.medinet
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        scanQrCodeButton.setOnClickListener { switchToQrScanner() }
+        requestCameraPermission()
     }
+
+    private fun requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 50)
+        }
+    }
+
+    private fun switchToQrScanner() {
+        val intent = Intent(this, CodeScannerActivity::class.java)
+        startActivity(intent)
+    }
+
 }
